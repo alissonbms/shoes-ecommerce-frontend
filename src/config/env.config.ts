a91c945cb6584931fa9442f4893c6ce2dcc6b354
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-26 15:41:22 -0300

    feat: add env config file

---
+++ src/config/env.config.ts
@@ -0,0 +1,8 @@
+// Guarda tudo quando o assunto é configuração de ambiente
+// E nossa url da api é uma configuração de ambiente
+
+const env = {
+  apiUrl: process.env.REACT_APP_API_URL
+}
+
+export default env
