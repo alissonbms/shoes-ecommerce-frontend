9beb6aba44803bb9ca4a047ad1299e23868ff028
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-27 14:25:21 -0300

    refactor: add Styled Components to Hero Component

--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -1,20 +1,18 @@
 import { FunctionComponent } from 'react'
-import './hero.styles.css'
+import { HeroTag, HomeText, Button } from './hero.styles'
 
 const Hero: FunctionComponent = () => {
   return (
-    <section className="hero">
-      <div className="home-text">
+    <HeroTag>
+      <HomeText>
         <h1>
           Comfortable <br />
           <span>and stylish</span>
         </h1>
         <p>Because what you wear has always been important</p>
-        <a href="#" className="btn">
-          View featured items
-        </a>
-      </div>
-    </section>
+        <Button href="#">View featured items</Button>
+      </HomeText>
+    </HeroTag>
   )
 }
 
---
+++ src/components/hero/hero.styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+import Colors from '../../theme/theme.colors'
+import Fonts from '../../theme/theme.fonts'
+import backgroundImage from '../../assets/background.jpg'
+
+export const HeroTag = styled.section`
+  padding: 100px 14%;
+  min-height: 100vh;
+  width: 100%;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center top;
+  display: grid;
+  align-items: center;
+`
+export const HomeText = styled.div`
+  h1 {
+    font-size: ${Fonts.big};
+    font-weight: 800;
+    line-height: 1.2;
+    letter-spacing: 0.2rem;
+    text-shadow: 2px 2px 3px black;
+
+    margin-bottom: 1.4rem;
+  }
+  span {
+    font-size: ${Fonts.h2Font};
+  }
+
+  p {
+    font-size: ${Fonts.normalFont};
+    font-weight: 500;
+    letter-spacing: 0.2rem;
+    margin-bottom: 3rem;
+  }
+`
+
+export const Button = styled.a`
+  display: inline-block;
+  background: ${Colors.background.linearGradient};
+  padding: 13px 25px;
+  color: ${Colors.text.white};
+  font-size: 18px;
+  letter-spacing: 1px;
+  font-weight: 600;
+  border-radius: 7px;
+  transition: all 0.35s ease;
+
+  &:hover {
+    transform: translateY(-10px);
+  }
+`
