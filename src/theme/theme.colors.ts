e8bd236d9f408539aa73cb4fd52856ab354ca01b
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-27 13:36:01 -0300

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  text: {
+    white: '#F8F9FA',
+    orange: '#ff521f',
+    linearGradient: 'linear-gradient(50deg, #dd2576, #ff521f)'
+  },
+  background: {
+    white: '#F8F9FA',
+    linearGradient: 'linear-gradient(50deg, #dd2576, #ff521f)'
+  },
+  input: {
+    background: '#E93CEF',
+    placeholder: '#6C757D'
+  },
+  error: '#FF6A6A'
+}
+
+export default Colors
