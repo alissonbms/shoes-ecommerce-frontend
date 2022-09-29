d1a41c19b5e8fc11200ae816b5f724e662dee221
Alisson da Silva <alissonnewdev@gmail.com>
2022-09-29 04:42:05 -0300

    feat: add new colors to Theme Colors file

--- src/theme/theme.colors.ts
+++ src/theme/theme.colors.ts
@@ -2,14 +2,16 @@ const Colors = {
   text: {
     white: '#F8F9FA',
     orange: '#ff521f',
-    linearGradient: 'linear-gradient(50deg, #dd2576, #ff521f)'
+    linearGradient: 'linear-gradient(50deg, #dd2576, #ff521f)',
+    dark: '#212529'
   },
   background: {
     white: '#F8F9FA',
-    linearGradient: 'linear-gradient(50deg, #dd2576, #ff521f)'
+    linearGradient: 'linear-gradient(50deg, #dd2576, #ff521f)',
+    dark: '#212529'
   },
   input: {
-    background: '#E93CEF',
+    background: '#E9ECEF',
     placeholder: '#6C757D'
   },
   error: '#FF6A6A'
