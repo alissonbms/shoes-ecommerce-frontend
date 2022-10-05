120e9b42c39e923a922b86d2839577a1ee21703c
Alisson da Silva <alissonnewdev@gmail.com>
2022-10-05 02:47:16 -0300

    feat: add user firestore converter and ensure User Context is updated on onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
 import { onAuthStateChanged } from 'firebase/auth'
-import { FunctionComponent } from 'react'
+import { getDocs, collection, where, query } from 'firebase/firestore'
+import { FunctionComponent, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { auth } from './config/firebase.config'
+
+// Utilities
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './contexts/user.context'
+import { userConverter } from './converters/firestore.converters'
 
 // Pages
 import HomePage from './pages/home/Home.page'
@@ -9,7 +17,24 @@ import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
 const App: FunctionComponent = () => {
-  onAuthStateChanged(auth, (user) => console.log(user))
+  const { isAuthenticated, login, logout } = useContext(UserContext)
+
+  onAuthStateChanged(auth, async (user) => {
+    if (isAuthenticated && !user) {
+      return logout()
+    }
+
+    if (!isAuthenticated && user) {
+      const querySnapshot = await getDocs(
+        query(
+          collection(db, 'users'),
+          where('id', '==', user.uid)
+        ).withConverter(userConverter)
+      )
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+      return login(userFromFirestore)
+    }
+  })
 
   return (
     <BrowserRouter>
